a84fcf80b18e61f8478aca28229589a3991a399e
RIKO OKADA <s1f101900206@iniad.org>
2021-11-20 20:02:03 +0900

    変更
    関数追加しました

--- pos-connected-app/resources/python/testReadCsv.py
+++ pos-connected-app/resources/python/testReadCsv.py
@@ -4,6 +4,7 @@ import sys
 # import boto3
 # import os
 # import io
+import week
 
 
 
@@ -11,16 +12,12 @@ import sys
 
 def main():
   args = sys.argv
-  # params = args[1].split(",")
-  # for i in args[1].split(","):
-  #   print(i)
-  #引数1 urls 引数2 3
-  print("profit,100000")
-  print("drink,3000,15")
-  print("yakitori,5000,15")
-  # df = pd.read_csv(url)
-  # print("test csv")
-  # print(df.head())
+  params = args[1].split(",")
+  csvs = []
+  for i in params:
+    csvs.append(i)
+  
+  week(csvs)
 
 
 if __name__ == "__main__":
