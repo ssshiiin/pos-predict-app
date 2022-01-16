0a1578110ae11a365b04cdd502d5383d48af0e51
KaitoToda <58159190+KaitoToda@users.noreply.github.com>
2022-01-16 14:25:04 +0900

    profit.blade

--- pos-connected-app/app/Http/Controllers/ProfitController.php
+++ pos-connected-app/app/Http/Controllers/ProfitController.php
@@ -7,16 +7,36 @@ use Illuminate\Http\Request;
 class ProfitController extends Controller
 {
     public function index(){
-        $test = [
+        $week = [
             "sun" => 120000,
             "mon" => 70000,
-            "thu" => 80000,
+            "tue" => 80000,
             "wed" => 40000,
-            "thr" => 80000,
+            "thu" => 80000,
             "fri" => 140000,
             "sat" => 200000,
         ];
 
-        return view("profit", ["data" => $test]);
+        return view("profit", ["data" => $week]);
     }
+    
+    public function index2(){
+        $month = [
+            "jan" => 800000,
+            "feb" => 700000,
+            "mar" => 1000000,
+            "apr" => 2000000,
+            "may" => 1800000,
+            "jun" => 1400000,
+            "jul" => 900000,
+            "aug" => 1000000,
+            "sep" => 1200000,
+            "oct" => 1400000,
+            "nov" => 1600000,
+            "dec" => 1800000,
+        ];
+
+        return view("profit", ["data2" => $month]);
+    }
+    
 }
--- pos-connected-app/resources/views/profit.blade.php
+++ pos-connected-app/resources/views/profit.blade.php
@@ -1,25 +1,92 @@
 @extends('layouts.index')
 @section("content")
-        profit
-        <div>
-            日曜日 : {{$data["sun"]}}
-        </div>
-        <div>
-            月曜日 : {{$data["mon"]}}
-        </div>
-        <div>
-            火曜日 : {{$data["thu"]}}
-        </div>
-        <div>
-            水曜日 : {{$data["wed"]}}
-        </div>
-        <div>
-            木曜日 : {{$data["thr"]}}
-        </div>
-        <div>
-            金曜日 : {{$data["fri"]}}
-        </div>
-        <div>
-            土曜日 : {{$data["sat"]}}
-        </div>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
+    <!-- bootstrap -->
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+    <br>
+    <br>
+
+    <!-- chart.js -->
+    <canvas id="myChart"></canvas>
+
+    <script>
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            
+            data: {
+                labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                datasets: [{
+                    label: '週間売上',
+                    backgroundColor: 'dodgerblue',
+                    borderColor: 'royalblue',
+                    data: [{{$data["sun"]}}, {{$data["mon"]}}, {{$data["tue"]}}, {{$data["wed"]}}, {{$data["thu"]}}, {{$data["fri"]}}, {{$data["sat"]}}]
+                }]
+            }, 
+
+            // Configuration options go here
+            options: {}
+        });
+
+    </script>
+
+        <br>
+        <table class = "table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col"></th>
+                <th scope="col"> Sun</th>
+                <th scope="col"> Mon</th>
+                <th scope="col"> Tue</th>
+                <th scope="col"> Wed</th>
+                <th scope="col"> Thu</th>
+                <th scope="col"> Fri</th>
+                <th scope="col"> Saty</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">売り上げ</th>
+                <td>{{$data["sun"]}}</td>
+                <td>{{$data["mon"]}}</td>
+                <td>{{$data["tue"]}}</td>
+                <td>{{$data["wed"]}}</td>
+                <td>{{$data["thu"]}}</td>
+                <td>{{$data["fri"]}}</td>
+                <td>{{$data["sat"]}}</td>
+                </tr>
+            </tbody>
+        </table>
+
+        
+        
+        
+        
+
+</body>
+</html>
 @endsection
