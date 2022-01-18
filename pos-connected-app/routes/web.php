72e0e3978a3e120be9a443df7b4c19f55624bc88
KaitoToda <58159190+KaitoToda@users.noreply.github.com>
2022-01-18 21:24:38 +0900

    dashboard変更

--- pos-connected-app/routes/web.php
+++ pos-connected-app/routes/web.php
@@ -16,7 +16,7 @@ Route::redirect('/', '/dashboard');
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
-Route::get('/dashboard', "DashboardController@index");
+//Route::get('/dashboard', "DashboardController@index");
 Route::get('/dashboard/profit', "ProfitController@index");
 //Route::get('/dashboard/profit', "ProfitController@index2");
 Route::get('/dashboard/product', "CsvsController@showProductWeek");
